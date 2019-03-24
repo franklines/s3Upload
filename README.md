16457efcd0de028b6255a1823d3f91f8e04a8436
franklines <escobarbfranklin@gmail.com>
2019-03-24 19:45:14 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# s3Upload
-A bash script to upload to AWS S3 bucket.
+# s3Upload.sh :floppy_disk:
+---
+s3Upload.sh is a bash script that you can use to upload a file to your AWS S3 bucket. Its scripted in pure bash and uses nothing but curl and openssl to sign and upload your request. This script was made to learn more about AWS V4 signature signing process and to get around situations where you can't install additional packages on a running system due to compliance or security reasons.
+
+#### Example usage:
+```bash
+./s3Upload.sh -b <bucket name> -l <file path without '/' at end> -f <file name> -r <region>
+
+./s3Upload.sh -b bucketofchickenwings -l /home/user -f dummyfile.txt -r us-east-1
+```
+
+Feel free to modify this script and use as you please. The script can be refactored/cleaned up. I was planning on adding an option to specify a folder within an S3 bucket and was also going to put everything into their own function. However, at this time the script itself does not serve me a purpose. It was done for education reasons. :)
+
+#### Huge shoutouts to the following resources:
+ - [Amazon S3 REST API with curl - Łukasz Adamczak](https://czak.pl/2015/09/15/s3-rest-api-with-curl.html)
+ - [Signature Version 4 Signing Process - Amazon Web Services](https://docs.aws.amazon.com/general/latest/gr/signature-version-4.html)
---
+++ s3Upload.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Author: Franklin E.
+# Description: This bash script uploads a file to AWS S3 using curl & openssl. It uses a AWSV4 signature, this is required for most regions that were launched during/after 2014.
+
+# AWS S3 Auth
+S3ACCESSKEY='<AWS Access Key>';
+S3SECRETKEY='<AWS Secret Key>';
+
+# Getopts parameters
+BUCKET="";
+LOCATION="";
+FILE="";
+REGION="";
+
+# Function to check our vars.
+function checkVars()
+{
+    if [ ! -z "$1" ];
+    then
+        echo "A parameter that is required was not specified! Please see usage example below.";
+	help
+        exit 1;
+    fi
+}
+
+# Help function
+function help()
+{
+    echo "Example usage:";
+    echo "./s3Upload.sh -b <bucket name> -l <file path without '/' at end> -f <file name> -r <region>";
+    echo "./s3Upload.sh -b bucketofchickenwings -l /home/user -f dummyfile.txt -r us-east-1";
+}
+
+# Getopts for our switches/flags. :)
+while getopts ":b::l:f::r:" opt; do
+  case $opt in
+    b)
+      BUCKET=$OPTARG >&2
+      ;;
+    l)
+      LOCATION=$OPTARG >&2
+      ;;
+    f)
+      FILE=$OPTARG >&2
+      ;;
+    r)
+      REGION=$OPTARG >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Verify getopts parameters.
+for var in "$BUCKET" "$LOCATION" "$FILE" "$REGION";
+do
+    checkVars "$var";
+done
+
+PAYLOAD=$(openssl dgst -sha256 $LOCATION/$FILE | sed 's/^.* //');
+AMZDATE=$(date +'%Y%m%dT%H%M%SZ');
+FETCHA=$(echo $AMZDATE | cut -dT -f1)
+
+# Canonical Request
+read -r -d '' CANREQ << EOF
+PUT
+/$FILE
+
+host:$BUCKET.s3.amazonaws.com
+x-amz-content-sha256:$PAYLOAD
+x-amz-date:$AMZDATE
+
+host;x-amz-content-sha256;x-amz-date
+$PAYLOAD
+EOF
+
+CANREQHASH=$(echo -n "$CANREQ" | openssl dgst -sha256 | sed 's/^.* //');
+
+# String to Sign
+read -r -d '' STRSIGN << EOF
+AWS4-HMAC-SHA256
+$AMZDATE
+$FETCHA/$REGION/s3/aws4_request
+$CANREQHASH
+EOF
+
+# Generate user's signing key.
+function hmac_sha256 {
+  key="$1"
+  data="$2"
+  echo -n "$data" | openssl dgst -sha256 -mac HMAC -macopt "$key" | sed 's/^.* //';
+}
+
+# Four-step signing key calculation
+dateKey=$(hmac_sha256 key:"AWS4$S3SECRETKEY" $FETCHA)
+dateRegionKey=$(hmac_sha256 hexkey:$dateKey $REGION)
+dateRegionServiceKey=$(hmac_sha256 hexkey:$dateRegionKey s3)
+signingKey=$(hmac_sha256 hexkey:$dateRegionServiceKey "aws4_request")
+
+
+# Calculate Signature
+SIGNATURE=$(echo -n "$STRSIGN" | openssl dgst -sha256 -mac HMAC -macopt hexkey:$signingKey | sed 's/^.* //');
+
+# Upload
+curl -sk -X PUT -T "$LOCATION/$FILE" \
+    -H "Authorization: AWS4-HMAC-SHA256 \
+    Credential=$S3ACCESSKEY/$FETCHA/$REGION/s3/aws4_request, \
+    SignedHeaders=host;x-amz-content-sha256;x-amz-date, \
+    Signature=$SIGNATURE" \
+    -H "x-amz-content-sha256: $PAYLOAD" \
+    -H "x-amz-date: $AMZDATE" \
+    "https://$BUCKET.s3.amazonaws.com/"
